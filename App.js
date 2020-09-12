da7f790752241f3467612c9b2e98114386fae8ae
yk-0496 <yeonseo1243@gmail.com>
2020-09-12 23:11:25 +0900

    Readme + App.json

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const {height, width} = Dimensions.get("window")
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>COOLTODOLIST</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.newinput} placeholder={"New To Do"} />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+    color:"white",
+    fontSize: 30,
+    marginTop: 50,  
+    fontWeight: "400",
+    marginBottom : 30
+  },
+  card :{
+    backgroundColor:"white",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius:10,
+    borderTopRightRadius:10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius:5,
+        shadowOffset:{
+          height:-1,
+          width:0
+        }
+        
+      },
+      android: {
+        elevation: 3
+
+      }
+    })
+
+  }
 });
